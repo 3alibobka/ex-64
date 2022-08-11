ed39fd86e60519845f9e97b38d247d93e726ce4d
Ivan <3alib0bka@mail.ru>
2022-08-11 18:09:53 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+for (int i = m; i <= n; i++)
+{
+Console.WriteLine(i);
+}
